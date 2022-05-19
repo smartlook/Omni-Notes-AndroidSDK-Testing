076ce6b2fec3e92d9b0cf005024c2642860681a7
frantisek <frantisek@smartlook.com>
2022-05-19 12:53:40 +0200

    Removed testing stuff

--- omniNotes/src/main/java/it/feio/android/omninotes/SensitivityPlayground.java
+++ omniNotes/src/main/java/it/feio/android/omninotes/SensitivityPlayground.java
@@ -1,12 +1,9 @@
 package it.feio.android.omninotes;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
-import com.smartlook.android.core.api.Smartlook;
 
 import it.feio.android.omninotes.smartlook.SmartlookHandler;
 
@@ -18,8 +15,5 @@ public class SensitivityPlayground extends AppCompatActivity {
         setContentView(R.layout.activity_sensitivity_playground);
 
         SmartlookHandler.onSensitivityPlayground(this);
-
-        Button a = findViewById(R.id.buttonA);
-        Smartlook.getInstance().getSensitivity().setViewInstanceSensitivity(a, true);
     }
 }
